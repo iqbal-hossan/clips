12f7dfa4285b0cdcaa9c01dd9ac839c78ee17b34
iqbal-hossan <iqbal.hossan@efgt.eurofoods-bd.com>
2023-10-10 12:54:08 +0600

    Resolved a CSS issue between parent and child components

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
@@ -9,9 +9,10 @@ import { ModalService } from 'src/app/services/modal.service';
 })
 export class ModalComponent implements OnInit {
   @Input() modalId = '';
-  constructor(public mdService: ModalService) { }
+  constructor(public mdService: ModalService, public el: ElementRef) { }
 
   ngOnInit(): void {
+    document.body.appendChild(this.el.nativeElement)
   }
 
   closeModal() {
